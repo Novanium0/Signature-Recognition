8b743ae3762cae6c18cbbe8b037fb0922140e11e
Novanium0 <76778222+Novanium0@users.noreply.github.com>
2021-05-28 14:22:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,30 @@
-##  Import:
-
 from tkinter import *
 
-##  On définit et on calibre notre fenêtre:
 fen=Tk()
 fen.title("Reconnaisance de signature")
 fen.geometry("1000x900")
-fen.config(bg='#808080')
+fen.config(bg='#000000')
 fen.resizable(width=False, height=False)
 
-##  On définit une zone pour la signature:
+can=Canvas(fen, width=1000, height=900, bg='#000000')
+next=Button(fen, text="Continuer", height=1, fg='#000000', bg='white', font=("Parchment",50), relief=RAISED, command=page1)
 
-zone=Frame(width=500, height=500, bg='#FFFFFF')
-zone.place(x=250, y=200)
-
-##  On place un titre:
-
-titre=Label(fen, text="Signez dans le cadre ci-dessous", height=2, fg='#FFFFFF', bg='#808080', font=("Parchment",50))
-titre.place(x=200, y=0)
-
-##  On demande si la personne souhaite conserver cette signature:
-
-confirmation=Label(fen, text="Etes-vous satifait(e) de votre signature?", height=1, fg='#FFFFFF', bg='#808080', font=("Parchment",50))
-confirmation.place(x=117, y=677)
-
-##  On définit les boutons pour répondre:
+def page0():
+    home1=can.create_text(500, 200, text="Projet d'informatique", fill="white", font=("Parchment",80))
+    home2=can.create_text(500, 450, text="La reconnaissance de signature", fill="white", font=("Parchment",80))
+    home3=can.create_window(500, 700, window=next)
 
 OUI=Button(fen, text="oui", height=1, fg='#000000', bg='#00FF00', font=("Parchment",25), relief=RAISED)
-OUI.place(x=365, y=777)
+NON=Button(fen, text="non", height=1, fg='#000000', bg='#FF0000', font=("Parchment",25), relief=RAISED)
 
-NON=Button(fen, text="non", height=1, fg='#000000', bg='#FF0000', font=("Parchment",25), relief=RAISED, command=non)
-NON.place(x=735, y=777)
+def page1():
+    can.delete("all")
+    titre=can.create_text(500, 100, text="Signez dans le cadre ci-dessous", fill='white', font=("Parchment",50))
+    confirmation=can.create_text(500, 700, text="Etes-vous satifait(e) de votre signature?", fill='white', font=("Parchment",50))
+    bouton1=can.create_window(350, 800, window=OUI)
+    bouton2=can.create_window(650, 800, window=NON)
+    zone=can.create_rectangle(300,200,700,600, fill='white')
 
-def non():
-    zone.pack_forget()
-
+page0()
+can.pack()
 fen.mainloop()
